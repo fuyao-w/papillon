4d7ec01a2526f17ea5064790fbb3052ffbb96d86
fuyao-w <815775754@qq.com>
2023-05-10 10:56:02 +0800

    feat:mem_snapshot_test

---
+++ mem_snapshot_test.go
@@ -0,0 +1,42 @@
+package papillon
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"reflect"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	Convey("create", t, func() {
+		content := []byte("a,b,c,d")
+		meta := &SnapShotMeta{
+			Version: 1,
+			ID:      "",
+			Index:   1,
+			Term:    1,
+			Configuration: Configuration{
+				[]ServerInfo{{
+					Voter, "aa", "xxx",
+				}},
+			},
+			ConfigurationIndex: 11,
+			Size:               0,
+		}
+		snapshot := newMemSnapShot()
+		sink, _ := snapshot.Create(meta.Version, meta.Index, meta.Term, meta.Configuration, meta.ConfigurationIndex, nil)
+		sink.Write(content)
+
+		sink.Cancel()
+		list, _ := snapshot.List()
+		So(len(list), ShouldEqual, 1)
+		newMeta := *list[0]
+		newMeta.Size = 0
+		newMeta.ID = ""
+		So(reflect.DeepEqual(&newMeta, meta), ShouldBeTrue)
+
+		meta, reader, _ := snapshot.Open(list[0].ID)
+		buf := make([]byte, meta.Size)
+		reader.Read(buf)
+		So(reflect.DeepEqual(content, buf), ShouldBeTrue)
+	})
+}
