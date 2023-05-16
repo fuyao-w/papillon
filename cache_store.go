ce30a09b72aacaba3b4763461efdc9b51a7c4f7b
fuyao-w <815775754@qq.com>
2023-05-16 21:00:39 +0800

    feat:cache store

--- cache_store.go
+++ cache_store.go
@@ -4,10 +4,11 @@ import (
 	. "github.com/fuyao-w/common-util"
 )
 
-// CacheLog 带缓存的 LogStore 用于减少磁盘 IO，只在 SetLogs, DeleteRange 时更新 cache 以保证局部性
+// CacheLog 带缓存的 LogStore 用于减少磁盘 IO，只在执行 SetLogs, DeleteRange 时更新 cache 以保证局部性
 type CacheLog struct {
-	store  LogStore
-	buffer *LockItem[[]*LogEntry]
+	store    LogStore
+	buffer   *LockItem[[]*LogEntry]
+	capacity uint64
 }
 
 // NewCacheLog capacity 必须大于 0
@@ -16,8 +17,9 @@ func NewCacheLog(store LogStore, capacity uint64) LogStore {
 		panic("capacity must bigger than 0")
 	}
 	return &CacheLog{
-		store:  store,
-		buffer: NewLockItem(make([]*LogEntry, capacity)),
+		store:    store,
+		buffer:   NewLockItem(make([]*LogEntry, capacity)),
+		capacity: capacity,
 	}
 }
 func (c *CacheLog) FirstIndex() (uint64, error) {
@@ -29,9 +31,9 @@ func (c *CacheLog) LastIndex() (uint64, error) {
 }
 
 func (c *CacheLog) GetLog(index uint64) (log *LogEntry, err error) {
-	buf := *c.buffer.Lock()
-	defer c.buffer.Unlock()
-	log = buf[index%uint64(len(buf))]
+	c.buffer.Action(func(t *[]*LogEntry) {
+		log = (*t)[index%c.capacity]
+	})
 	if log != nil && log.Index == index {
 		return
 	}
@@ -40,16 +42,17 @@ func (c *CacheLog) GetLog(index uint64) (log *LogEntry, err error) {
 
 func (c *CacheLog) GetLogRange(from, to uint64) (logs []*LogEntry, err error) {
 	buf := *c.buffer.Lock()
-	defer c.buffer.Unlock()
 	for i := from; i <= to; i++ {
-		if log := buf[i%uint64(len(buf))]; log != nil && log.Index == i {
+		if log := buf[i%c.capacity]; log != nil && log.Index == i {
 			logs = append(logs, log)
 		} else {
 			goto LOAD
 		}
 	}
+	c.buffer.Unlock()
 	return
 LOAD:
+	c.buffer.Unlock()
 	return c.store.GetLogRange(from, to)
 }
 
@@ -57,11 +60,11 @@ func (c *CacheLog) SetLogs(logs []*LogEntry) error {
 	if err := c.store.SetLogs(logs); err != nil {
 		return err
 	}
-	buf := *c.buffer.Lock()
-	defer c.buffer.Unlock()
-	for _, log := range logs {
-		buf[log.Index%uint64(len(buf))] = log
-	}
+	c.buffer.Action(func(buf *[]*LogEntry) {
+		for _, log := range logs {
+			(*buf)[log.Index%c.capacity] = log
+		}
+	})
 	return nil
 }
 
@@ -69,13 +72,14 @@ func (c *CacheLog) DeleteRange(from, to uint64) error {
 	if err := c.store.DeleteRange(from, to); err != nil {
 		return err
 	}
-	buf := *c.buffer.Lock()
-	defer c.buffer.Unlock()
-	for i := from; i <= to; i++ {
-		idx := i % uint64(len(buf))
-		if log := buf[idx]; log != nil && log.Index == i {
-			buf[idx] = nil
+	c.buffer.Action(func(buf *[]*LogEntry) {
+		for i := from; i <= to; i++ {
+			idx := i % c.capacity
+			if log := (*buf)[idx]; log != nil && log.Index == i {
+				(*buf)[idx] = nil
+			}
 		}
-	}
+	})
+
 	return nil
 }
